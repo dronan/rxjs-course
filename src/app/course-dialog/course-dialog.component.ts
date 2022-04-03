3582d082e4535c264dccc929054424730e554bd1
DIego Ronan <dronan@gmail.com>
2022-04-03 14:34:31 +0200

    exhaustMap para bloquear multiplas requisicoes iguais, como varios clicks em um save, ele só considera o envio de 1

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -63,6 +63,10 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
 
     ngAfterViewInit() {
 
+            fromEvent(this.saveButton.nativeElement, 'click').pipe(
+                exhaustMap( () => this.saveCourse(this.form.value) )
+            )
+            .subscribe()
 
     }
 
