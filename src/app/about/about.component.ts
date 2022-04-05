d907117fcb713371b274e542e319ef1d240aa2d6
DIego Ronan <dronan@gmail.com>
2022-04-05 11:01:48 +0200

    testes com BehaviorSubject e Subject

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, BehaviorSubject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -13,6 +13,24 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
+        const subject = new Subject();
+        // const subject = new BehaviorSubject(); // guarda o ultimo valor associado para colocar na proxima subscricao
+
+        const serie$ = subject.asObservable();
+
+        serie$.subscribe( (val) => console.log(`inicio do subs`, val) )
+
+        subject.next(1);
+        subject.next(2);
+        subject.next(3);
+
+        subject.complete();
+
+        setTimeout( () => {
+            serie$.subscribe( (val) => console.log(`sub pos 3s`, val) )
+            subject.next(4);
+
+        }, 3000)
 
     }
 
