5f2291f91a2422975a667274de3446cdf70f2af6
DIego Ronan <dronan@gmail.com>
2022-04-05 10:39:09 +0200

    exemplo do forkjoin - execucao de observables em paralelo e exibicao final da ultima execucao de cada

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -13,7 +13,7 @@ import {
     withLatestFrom,
     concatAll, shareReplay
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
+import {merge, fromEvent, Observable, concat, forkJoin} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservable } from '../common/util';
 import { debug, RxJsLoggingLevel } from '../common/debug';
@@ -43,25 +43,31 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
         this.courseId = this.route.snapshot.params['id'];
 
-        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`).pipe(
-            debug( RxJsLoggingLevel.INFO, "course"),
-        );
+        const course$ = createHttpObservable(`/api/courses/${this.courseId}`)
  
+        const lessons$ = this.loadLessons();
+
+        forkJoin(course$, lessons$).pipe(
+            tap( ([course, lesson]) => {
+                console.log(course);
+                console.log(lesson);
+                
+
+            })
+
+        ).subscribe(console.log)
 
     }
 
     ngAfterViewInit() {
 
-        this.lessons$ = fromEvent(this.input.nativeElement, 'keyup')
+       fromEvent(this.input.nativeElement, 'keyup')
         .pipe(
             map((event: any) => event.target.value),
             startWith(''),
-            debug( RxJsLoggingLevel.INFO, "search"),
             debounceTime(500),
             distinctUntilChanged(),
             switchMap(search => this.loadLessons(search)),
-            debug( RxJsLoggingLevel.INFO, "lessons"),
-
         )
 
     }
@@ -70,7 +76,6 @@ export class CourseComponent implements OnInit, AfterViewInit {
         return createHttpObservable(`/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${search}`)
         .pipe(
             map(res => res['payload'])
-            
         );
     }
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,7 +25,6 @@ export class HomeComponent implements OnInit {
         
         const courses$: Observable<Course[]> = http$.pipe(
             map( res => res['payload'] ),
-            tap( (oi) => console.log('post map execution', oi)),
             shareReplay(),
             retryWhen( (errors) => errors.pipe( delayWhen( () => timer(2000) ) ) )
 
